79b626eb1edc2ea4c973087b56e2d6cd5578349d
keke bargallo <kekebargallo@gmail.com>
2022-08-30 17:21:26 -0300

    nav added

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+const navBar = () => {
+    return <div class="navContainer">
+            <nav class="nav">
+
+        <ul class="navList">
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">
+                Shop
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                Galería
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                Sobre nosotros
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                Contactos
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+</div>
+};
+
+export default navBar;
